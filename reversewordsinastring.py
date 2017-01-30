98e96ba4f1bfe5dff33b3ef1bf0550f8b8fbd480
sctsai <john15518513@gmail.com>
2017-01-29 18:53:09 -0600

    151

---
+++ reversewordsinastring.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = s.split()
+        return " ".join(res[::-1])
