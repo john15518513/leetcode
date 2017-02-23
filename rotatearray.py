9e6fccf99499ea4a8ea4ba7520183dd57ce6d675
sctsai <john15518513@gmail.com>
2017-02-23 17:12:38 -0600

    189

---
+++ rotatearray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k%n
+        nums[:] = [ nums[(n-k+i)%n] for i in xrange(n) ]
+            
