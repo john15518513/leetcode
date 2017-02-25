da4fbce0a564104d61c9fe4778430a4400be7059
sctsai <john15518513@gmail.com>
2017-02-25 01:27:19 -0600

    101

---
+++ symmetrictree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        def helper(left, right):
+            if not left or not right:
+                return left == right
+            if left.val != right.val:
+                return False
+            return helper(left.left, right.right) and helper(left.right, right.left)
+            
+            
+        return helper(root.left, root.right)
+    
+    
