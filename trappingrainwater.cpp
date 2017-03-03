f8812c23717a18f4f150bec4a8cf4572a0b16034
sctsai <john15518513@gmail.com>
2017-03-03 02:43:14 -0600

    42

---
+++ trappingrainwater.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int leftMax = 0;
+        int rightMax = 0;
+        int left = 0;
+        int right = height.size()-1;
+        int res = 0;
+        while (left<=right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) leftMax = height[left];
+                else res += leftMax-height[left];
+                left++;
+            }
+            else {
+                if (height[right] >= rightMax) rightMax = height[right];
+                else res += rightMax-height[right];
+                right--;
+            }
+        }
+        return res;
+    }
+};
