5e60d0d5a5b170efc3aac85ad95ed5eee16f297e
sctsai <john15518513@gmail.com>
2017-03-03 10:26:09 -0600

    13

---
+++ romantointeger.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map <char, int> hash = { { 'I' , 1 },
+                                   { 'V' , 5 },
+                                   { 'X' , 10 },
+                                   { 'L' , 50 },
+                                   { 'C' , 100 },
+                                   { 'D' , 500 },
+                                   { 'M' , 1000 } };
+        
+        int res = 0;
+        for (int i = 0; i < s.length()-1; i++) {
+            if (hash[s[i]] >= hash[s[i+1]]) {
+                res += hash[s[i]];
+            }
+            else {
+                res -= hash[s[i]];
+            }
+        }
+        return res + hash[s.back()];
+    }
+};
