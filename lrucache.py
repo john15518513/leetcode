1fbf6a5f3e34918b0c5d7bcab1edead0b8520498
sctsai <john15518513@gmail.com>
2017-03-20 00:17:48 -0500

    146

--- lrucache.py
+++ lrucache.py
@@ -44,3 +44,41 @@ class LRUCache(object):
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.dict = collections.OrderedDict()
+        
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.dict:
+            return -1
+        value = self.dict.pop(key)
+        self.dict[key] = value
+        return self.dict[key]
+        
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key in self.dict:
+            self.dict.pop(key)
+        if key not in self.dict and len(self.dict) == self.capacity:
+            self.dict.popitem(last=False)
+        
+        self.dict[key] = value
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
