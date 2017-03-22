d45ecc32ec3103f07fa67b56cc9a316c753a5125
sctsai <john15518513@gmail.com>
2017-03-22 16:36:34 -0500

    189

--- rotatearray.py
+++ rotatearray.py
@@ -8,4 +8,24 @@ class Solution(object):
         n = len(nums)
         k = k%n
         nums[:] = [ nums[(n-k+i)%n] for i in xrange(n) ]
-            
+    
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = k%len(nums)
+        def reverse(nums, i, j):
+            while i<j:
+                tmp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = tmp
+                i += 1
+                j -= 1
+                
+        reverse(nums, 0, len(nums)-1)
+        reverse(nums, 0, k-1)
+        reverse(nums, k, len(nums)-1)
+        
+                
