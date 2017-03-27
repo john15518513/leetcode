e5e0a09d61b94df4e47dabbd194e3b1d0a0af490
sctsai <john15518513@gmail.com>
2017-03-27 18:49:31 -0500

    15

--- 3sum.py
+++ 3sum.py
@@ -29,3 +29,34 @@ class Solution(object):
                     j += 1
 
         return list(set(ans)) # I bet this is not the best way to eliminate duplicate solutions
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 3 or (len(nums) == 3 and sum(nums) != 0):
+            return []
+        nums.sort()
+        res = []
+        #print "hi"
+        for i in xrange(len(nums)-2):
+            if i == 0 or nums[i]!=nums[i-1]:
+                j, k = i+1, len(nums)-1
+                while j<k:
+                    tmp = nums[i]+nums[j]+nums[k]
+                    if tmp == 0:
+                        res.append((nums[i], nums[j], nums[k]))
+                        while j < k and nums[j] == nums[j+1]:
+                            j += 1
+                        while j < k and nums[k] == nums[k-1]:
+                            k -= 1
+                        j += 1
+                        k -= 1
+                    elif tmp > 0:
+                        k -= 1
+                    else:
+                        j += 1
+        return list(set(res))
+            
