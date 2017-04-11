6a7e07156b4895fc1cb95b398ebb70c3ade95390
sctsai <john15518513@gmail.com>
2017-04-10 22:43:49 -0500

    63

---
+++ uniquepathsii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        
+        dp = [[0 for _ in xrange(len(obstacleGrid[0])+1)] for _ in xrange(len(obstacleGrid)+1)]
+        dp[0][1] = 1
+        for i in xrange(len(obstacleGrid)):
+            for j in xrange(len(obstacleGrid[0])):
+                if obstacleGrid[i][j]!=1:
+                    dp[i+1][j+1] = dp[i][j+1]+dp[i+1][j]
+        return dp[-1][-1]
