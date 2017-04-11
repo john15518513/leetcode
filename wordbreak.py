f0aceeb13ab745923723b95197f3ac17b00c40f0
sctsai <john15518513@gmail.com>
2017-04-11 16:32:16 -0500

    139

---
+++ wordbreak.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        
+        dp = [False]*len(s)
+        for i in xrange(len(s)):
+            for w in wordDict:
+                if w == s[i-len(w)+1:i+1] and (dp[i-len(w)] or i-len(w) == -1):
+                    dp[i] = True
+        return dp[-1]
