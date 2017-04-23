0d3e625e60d6c30a9153d47814e39ff7ec7d121a
sctsai <john15518513@gmail.com>
2017-04-23 17:45:56 -0500

    124

---
+++ binarytreemaximumpathsum.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.maxValue = float('-inf')
+        self.maxPathDown(root)
+        return self.maxValue
+    
+    def maxPathDown(self, root):
+        if not root: return 0
+        left = max(0, self.maxPathDown(root.left))
+        right = max(0, self.maxPathDown(root.right))
+        self.maxValue = max(self.maxValue, left+right+root.val)
+        return max(left, right) + root.val
+        
