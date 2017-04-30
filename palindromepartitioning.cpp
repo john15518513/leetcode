8dcceff5ae060d2195292d19e8bb0d1f88e54aca
sctsai <john15518513@gmail.com>
2017-04-30 16:02:34 -0500

    131

---
+++ palindromepartitioning.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<string> tmp;
+        for (int i = 0; i < s.length(); i++) {
+            dfs(s.substr(0, i+1), tmp, 0, s.length());
+        }
+        return res;
+    }
+    void dfs(string s, vector<string> &tmp, int len, int total) {
+        if (len == total) {
+            res.push_back(tmp);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            string ss = s.substr(0,i+1);
+            string rs = s.substr(0,i+1);
+            reverse(rs.begin(), rs.end());
+            if (ss == rs) {
+                tmp.push_back(ss);
+                dfs(s.substr(ss.length(), s.length()-ss.length()), tmp, len+i+1, total);
+                tmp.pop_back();
+            }
+        }
+    }
+    
+private:
+    vector<vector<string>> res;
+};
