407924e45ecbaa3336b577614a13385fdb980440
sctsai <john15518513@gmail.com>
2017-05-03 17:29:56 -0500

    459

---
+++ repeatingsubstringpattern.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int len = s.length()/2;
+        for (int i = len-1; i >= 0; i--) {
+            string token = s.substr(0,i+1);
+            //cout << "token: "<< token << " ";
+            int pos = i+1;
+            //cout << "len: " << i+1 << "pos: " << pos << endl;
+            while (true) {
+                if (pos == s.length()) return true;
+                if (pos+i+1 > s.length()) break;
+                if (token != s.substr(pos, i+1))break;
+                pos += i+1;
+            }
+        }
+        return false;
+    }
+};
