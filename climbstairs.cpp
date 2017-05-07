76d4a80091d475eafadbd1b7dfe28f6987b93ef6
sctsai <john15518513@gmail.com>
2017-05-07 18:22:53 -0500

    70

---
+++ climbstairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int n1 = 1, n2 = 2;
+        int res;
+        for (int i = 3; i <= n; i++) {
+            res = n1+n2;
+            swap(n1,n2);
+            n2 = res;
+        }
+        return res;
+    }
+};
