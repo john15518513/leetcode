116518c404ea98f86e18c7d8936289346e237c69
sctsai <john15518513@gmail.com>
2017-05-15 00:00:05 -0500

    215

--- kthlargestelementinanarray.cpp
+++ kthlargestelementinanarray.cpp
@@ -13,3 +13,16 @@ public:
         return minHeap.front()*-1;
     }
 };
+
+//PQ solution
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto &n: nums) {
+            pq.push(n);
+            if (pq.size() > k) pq.pop();
+        }
+        return pq.top();
+    }
+};
