d082e2fa4cfb31778536121d4663f42a0c2d7ca3
sctsai <john15518513@gmail.com>
2017-06-03 15:57:55 -0500

    581

--- shortestunosrtedontinuoussubarray.cpp
+++ shortestunosrtedontinuoussubarray.cpp
@@ -28,3 +28,19 @@ public:
         return right-left+1;
     }
 };
+
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int left = -1, right = -2;
+        int minV = nums.back(), maxV = nums.front();
+        int n = nums.size();
+        for (int i = 1; i < nums.size(); i++) {
+            maxV = max(maxV, nums[i]);
+            minV = min(minV, nums[n-1-i]);
+            if (nums[i] < maxV) right = i;
+            if (nums[n-1-i] > minV) left = n-1-i; 
+        }
+        return right-left+1;
+    }
+};
