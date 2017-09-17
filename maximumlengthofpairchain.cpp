e261203ef5670d9b8bde21210a5c479dc830502a
sctsai <john15518513@gmail.com>
2017-09-17 13:31:10 -0500

    646

--- maximumlengthofpairchain.cpp
+++ maximumlengthofpairchain.cpp
@@ -15,3 +15,26 @@ public:
         return a[1] < b[1];
     }
 };
+
+//DP Solution
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), cmp);
+        int maxV = 1;
+        vector<int> dp(pairs.size(), 1);
+        for (int i = 1; i < pairs.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[i][0] > pairs[j][1]) {
+                    dp[i] = max(dp[i], dp[j]+1);  
+                }
+            }
+            maxV = max(maxV, dp[i]);
+        }
+        return maxV;
+        
+    }
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        return a[0] < b[0];
+    }
+};
