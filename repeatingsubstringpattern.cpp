8480f2e9dd154a8f62b6ce988cd2a2f8623fbb00
sctsai <john15518513@gmail.com>
2017-09-21 12:53:17 -0500

    459

--- repeatingsubstringpattern.cpp
+++ repeatingsubstringpattern.cpp
@@ -17,3 +17,24 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        /*int len = s.length()/2;
+        for (int i = len-1; i >= 0; i--) {
+            string token = s.substr(0,i+1);
+            int pos = i+1;
+            while (true) {
+                if (pos == s.length()) return true;
+                if (pos+i+1 > s.length()) break;
+                if (token != s.substr(pos, i+1))break;
+                pos += i+1;
+            }
+        }
+        return false;*/
+        string ss = s + s;
+        //cout << ss.find(s, 1)  << " "  << s.length() << endl;
+        return ss.find(s, 1) < s.size();
+    }
+};
