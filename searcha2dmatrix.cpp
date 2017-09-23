efb73b1c55b1a768e0360612c4f28a648f66f0d6
sctsai <john15518513@gmail.com>
2017-09-23 16:36:48 -0500

    74

--- searcha2dmatrix.cpp
+++ searcha2dmatrix.cpp
@@ -15,3 +15,21 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        if (n == 0) return false;
+        int m = matrix[0].size();
+        int l = 0, r = m*n-1;
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            int midV = matrix[mid/m][mid%m];
+            if ( midV == target) return true;
+            else if (midV > target) r = mid-1;
+            else l = mid+1;
+        }
+        return false;
+    }
+};
