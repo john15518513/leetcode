6fd4aefd090dd280ba9902c8d8d99281280e965c
sctsai <john15518513@gmail.com>
2017-10-03 20:02:40 -0500

    15

--- 3sum.cpp
+++ 3sum.cpp
@@ -25,3 +25,30 @@ public:
         return res;
     }
 };
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if (nums.size() < 3) return res;
+        sort(nums.begin(), nums.end(), [](const int &i, const int &j){return i<j;});
+        for (int i = 0; i < nums.size()-2; i++) {
+            int j = i+1, k = nums.size()-1;
+            if (i != 0 and nums[i] == nums[i-1]) continue;
+            while (j<k) {
+                int tmp = nums[i] + nums[j] + nums[k];
+                if (tmp == 0) {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k and nums[j] == nums[j+1]) j++;
+                    while (j < k and nums[k] == nums[k-1]) k--;
+                }
+                if (tmp < 0) {
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+};
