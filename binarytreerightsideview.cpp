b44be72af0ec140e4ab5b1be699e1eee93dfe8de
sctsai <john15518513@gmail.com>
2017-10-07 18:26:35 -0500

    199

---
+++ binarytreerightsideview.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        dfs(root, res, 0);
+        return res;
+    }
+    void dfs(TreeNode* root, vector<int>& res, int level) {
+        if (!root) return;
+        if (res.size() == level) res.push_back(root->val);
+        dfs(root->right, res, level+1);
+        dfs(root->left, res, level+1);
+    }
+};
