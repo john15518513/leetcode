f9ffc3df0f60f482365a8be72e104b53709f4f95
sctsai <john15518513@gmail.com>
2017-10-14 19:51:15 -0500

    139

--- wordbreak.cpp
+++ wordbreak.cpp
@@ -18,3 +18,19 @@ public:
         return dp.back();
     }
 };
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> words;
+        for (auto &w: wordDict) words.insert(w);
+        vector<bool> dp(s.length(), false);
+        for (int i = 0; i < s.size(); i++) {
+            for (int j = 1; j <= s.size(); j++) {
+                if (j+i-1 >= s.size()) break;
+                if ((i == 0 || dp[i-1] == true) and words.find(s.substr(i, j)) != words.end()) dp[i+j-1] = true;
+            }
+        }
+        return dp.back();
+    }
+};
