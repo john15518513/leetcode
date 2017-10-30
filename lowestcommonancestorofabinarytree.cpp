00d3addea9e4cd0341cd825a6558d383508d456a
sctsai <john15518513@gmail.com>
2017-10-29 22:49:29 -0500

    236

--- lowestcommonancestorofabinarytree.cpp
+++ lowestcommonancestorofabinarytree.cpp
@@ -19,3 +19,43 @@ public:
         return NULL;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> que;
+        unordered_map<TreeNode*, TreeNode*> map;
+        map[root] = NULL;
+        que.push(root);
+        while (!que.empty()) {
+            auto target = que.front();
+            que.pop();
+            if (target->left) {
+                map[target->left] = target;
+                que.push(target->left);
+            }
+            if (target->right) {
+                map[target->right] = target;
+                que.push(target->right);
+            }
+        }
+        unordered_set<TreeNode*> set;
+        while (p != NULL) {
+            set.insert(p);
+            p = map[p];
+        }
+        while (q != NULL) {
+            if (set.find(q) != set.end()) return q;
+            q = map[q];
+        }
+    }
+};
