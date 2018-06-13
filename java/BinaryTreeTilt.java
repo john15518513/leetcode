01415a94d755d771a464b5c729345e830c945d0f
sctsai <john15518513@gmail.com>
2018-06-12 21:53:01 -0400

    563

---
+++ java/BinaryTreeTilt.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int findTilt(TreeNode root) {
+        dfs(root);
+        return sum;
+    }
+    
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        sum += Math.abs(left - right);
+        return root.val + left + right;
+    }
+}
